4d1fa36f264886e13c67da5025e18c4794d48457
GitHub Action <action@github.com>
2022-02-10 19:46:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sheanhoxie/landophpstormdb/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sheanhoxie.landophpstormdb.services
+
+import com.github.sheanhoxie.landophpstormdb.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
